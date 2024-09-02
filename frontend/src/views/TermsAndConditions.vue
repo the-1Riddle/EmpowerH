a5cd9f2771334a4b6c81346c2916c2b855516bb2
Elvis Otieno <125451537+the1Riddle@users.noreply.github.com>
2024-09-02 17:25:52 +0000

    frontend

--- frontend/src/views/TermsAndConditions.vue
+++ frontend/src/views/TermsAndConditions.vue
@@ -7,6 +7,29 @@
             <div class="mt-150">
             	<div class="container">
             		<h3 style="text-align:center">Terms And <span class="orange-green-text">Conditions</span></h3>
+                    <hr>
+
+                    <br>
+                    <h4>Thank you for using EmpowerHer!</h4>
+                    <p>These Terms of Service (“Terms”) govern your access to and use of any website, app, service, technology, API, widget, platform, channel or any other products or features owned, operated, branded or offered by EmpowerHer (“EmpowerHer” or the “Service”), except where we expressly state that separate terms (and not these) apply. For the purposes of these Terms, “we” or “us” refers to the party with whom you are entering into this agreement, per Section 13(e) (Parties). Please read these Terms carefully, and contact us if you have any questions.
+
+You may not use the Service to do or share anything that is contrary to these Terms. For clarity, these Terms include, and incorporate by reference, the following policies: <br><br>
+
+
+                    <ul>
+                        <li>Our Privacy Policy, which explains how we collect, use, and share information.</li>
+                        <li>Our Community Guidelines, which explain what is and isn’t allowed on EmpowerHer.</li>
+                        <li>Our Advertising Guidelines and Merchant Guidelines, which explain additional policies that apply to ads and merchants on EmpowerHer.</li>
+                        <li>Our Enforcement practices, which explain how we put our policies into practice, including restrictions we may impose on your content or use of EmpowerHer.</li>
+                    </ul>
+                    <br><br>
+
+    By accessing or using EmpowerHer, you agree to be bound by these Terms. If you do not agree to our Terms, you must not access or use EmpowerHer.
+
+More simply put
+
+Every company has its terms. These are ours. They include these Terms of Service and other policies listed above.
+                    </p>
             	</div>
             </div>
         </section>
